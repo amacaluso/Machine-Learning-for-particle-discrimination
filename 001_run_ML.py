670cb7b240cd43dc64d6ada20bf29efefc002911
amacaluso <a.macaluso@cineca.it>
2018-08-23 09:11:15 +0200

    ML OK

--- 001_run_ML.py
+++ 001_run_ML.py
@@ -9,7 +9,7 @@ njob = 16
 # exec(open("035_UNIVARIATE_VARIABLES_SELECTION.py").read(), globals())
 
 #methods = ['ISIS', 'LASSO', 'DECISION_TREE',
-methods = ['RANDOM_FOREST', 'GBM', 'E_NET', 'INFORMATION_GAIN', 'LR_ACCURACY', 'ISIS', 'LASSO']
+methods = [ 'LR_ACCURACY', 'E_NET', 'INFORMATION_GAIN', 'ISIS', 'LASSO', 'RIDGE' 'DECISION_TREE', 'RANDOM_FOREST', 'GBM']
 #method = 'ISIS'
 # GET PREDICTOR
 # ['LASSO', 'DECISION_TREE', 'RANDOM_FOREST', 'GBM',
@@ -24,32 +24,38 @@ DF = pd.DataFrame()
 
 
 
-for method in methods:
-    predictors = extract_predictors(method, 1, SEED)
+#for method in methods:
+
+method = methods[2]
+nvars = []
+all_nvars = np.concatenate( ([1], np.arange(10, 51, 10), np.arange(70, 130, 30)))
+
+for i in all_nvars:
+    predictors = extract_predictors(method, i, SEED)
     eff_nvar = len(predictors)
-    nvars = [eff_nvar, 1, 3, 5, 10, 15, 30, 50, 70, 100] #, 130]
-    nvars = list(set( [el for el in nvars if el>=eff_nvar] ))
-    for nvar in nvars:
-        #nvar = nvars[0]
-        predictors = extract_predictors(method, nvar, SEED)
-        eff_nvar = len(predictors)
-        if nvar >= eff_nvar:
-            print method, nvar
-            try:
-                exec(open("041_TREE_BASED_MODELS.py").read(), globals())
-            except:
-                DF.to_csv('000_TREE_BASED_' + method + '_' + str(nvar) + '.csv')
-            # >>>>>>>>> exec(open("042_SVM.py").read(), globals()) >>>>>>>>>>>>
-            try:
-                exec(open("043_REGULARIZED_METHODS.py").read(), globals())
-            except:
-                DF.to_csv('000_LASSO_' + method + '_' + str(nvar) + '.csv')
-            try:
-                exec(open("045_NAIVE_BAYES.py").read(), globals())
-            except:
-                DF.to_csv('000_NAIVE_BAYES_' + method + '_' + str(nvar) + '.csv')
-            try:
-                exec(open("046_KNN.py").read(), globals())
-            except:
-                DF.to_csv('000_KNN_' + method + '_' + str(nvar) + '.csv')
-            DF.to_csv( '999_' + method + '_' + str(nvar) + '.csv' )
+    if eff_nvar not in nvars:
+        nvars.append( eff_nvar)
+    # nvars = list(set( [el for el in nvars if el>=eff_nvar] ))
+
+for nvar in nvars:
+    predictors = extract_predictors(method, nvar, SEED)
+    eff_nvar = len(predictors)
+    print method, nvar
+    try:
+        exec(open("041_TREE_BASED_MODELS.py").read(), globals())
+    except:
+        DF.to_csv('000_TREE_BASED_' + method + '_' + str(nvar) + '.csv')
+        # >>>>>>>>> exec(open("042_SVM.py").read(), globals()) >>>>>>>>>>>>
+    try:
+        exec(open("043_REGULARIZED_METHODS.py").read(), globals())
+    except:
+        DF.to_csv('000_LASSO_' + method + '_' + str(nvar) + '.csv')
+    try:
+        exec(open("045_NAIVE_BAYES.py").read(), globals())
+    except:
+        DF.to_csv('000_NAIVE_BAYES_' + method + '_' + str(nvar) + '.csv')
+    try:
+        exec(open("046_KNN.py").read(), globals())
+    except:
+        DF.to_csv('000_KNN_' + method + '_' + str(nvar) + '.csv')
+    DF.to_csv( '999_' + method + '_' + str(nvar) + '.csv' )
