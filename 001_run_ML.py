9ba62821c41acc2f42f10836da3d6e7d165c0e53
amacaluso <a.macaluso@cineca.it>
2018-09-04 13:11:29 +0200

    DL OK

--- 001_run_ML.py
+++ 001_run_ML.py
@@ -10,10 +10,10 @@ njob = 16
 
 #method = 'ISIS'
 # GET PREDICTOR ['ISIS', 'LR_ACCURACY', 'E_NET', 'INFORMATION_GAIN', 'LASSO', 'RIDGE', 'RANDOM_FOREST', 'GBM']
-# all_nvars = np.concatenate( ([1], np.arange(10, 51, 10))), np.arange(70, 130, 30)))
+all_nvars = np.concatenate( ([1], np.arange(10, 51, 10), np.arange(70, 130, 30)) )
 
 methods = ['DECISION_TREE', 'ISIS', 'LR_ACCURACY', 'E_NET', 'INFORMATION_GAIN', 'LASSO', 'RIDGE', 'RANDOM_FOREST', 'GBM']
-all_nvars = np.concatenate( ([1], np.arange(10, 51, 10)))
+#all_nvars = np.concatenate( ([1], np.arange(10, 51, 10)))
 
 
 # predictors = extract_predictors( method, nvar, SEED)
@@ -118,6 +118,74 @@ for method in methods:
         except:
             DF.to_csv( scheduled_model + '000_RF_GBM_' + method + '_' + str(nvar) + '.csv')
         DF.to_csv(  scheduled_model + '999_RF_GBM_' + method + '_' + str(nvar) + '.csv' )
+
+
+
+
+
+
+
+
+
+
+
+srun -N 1 -n16 -A cin_staff -t300  -p gll_usr_gpuprod --gres=gpu:kepler:2 --pty /bin/bash
+module use /gpfs/scratch/userinternal/epascol1/spack/share/spack/modules/linux-centos7-x86_64
+module load  gcc-6.3.0-gcc-7.3.0-us4i5fv
+module load  cuda-9.0.176-gcc-6.3.0-xjorzpo
+module load  cudnn-7.0.5-gcc-6.3.0-ceoy3cj
+module load python/2.7.12
+source py2/bin/activate
+cd INAF/
+python
+
+
+
+
+
+exec(open("Utils.py").read(), globals())
+exec(open("000_Utils_parallel.py").read(), globals())
+
+SEED = 741
+njob = 4
+
+# exec(open("015_SPLITTING_DATA.py").read(), globals())
+# exec(open("030_VARIABLES_SELECTION.py").read(), globals())
+# exec(open("035_UNIVARIATE_VARIABLES_SELECTION.py").read(), globals())
+
+#method = 'ISIS'
+# GET PREDICTOR ['ISIS', 'LR_ACCURACY', 'E_NET', 'INFORMATION_GAIN', 'LASSO', 'RIDGE', 'DECISION_TREE', 'RANDOM_FOREST', 'GBM']
+# all_nvars = np.concatenate( ([1], np.arange(10, 51, 10))), np.arange(70, 130, 30)))
+
+methods = ['LR_ACCURACY', 'E_NET', 'INFORMATION_GAIN', 'LASSO', 'RIDGE', 'RANDOM_FOREST', 'GBM']
+nvars = [30, 50] #np.arange(70, 140, 20).tolist()
+
+# ****************************************************#
+#all_nvars = [100]
+#methods = ['ISIS']
+# ****************************************************#
+
+# predictors = extract_predictors( method, nvar, SEED)
+# eff_nvar = len(predictors)
+
+probs_to_check = np.arange(0.1, 0.91, 0.1)
+DF = pd.DataFrame()
+
+scheduled_model = 'running_model/'
+create_dir( scheduled_model)
+
+method = 'LR_ACCURACY'
+
+for nvar in nvars:
+    # nvar = 100
+    predictors = extract_predictors(method, nvar, SEED)
+    eff_nvar = len(predictors)
+    print method, nvar
+    try:
+        exec(open("051_NEURAL_NETWORK.py").read(), globals())
+    except:
+        DF.to_csv( scheduled_model + '000_NN_' + method + '_' + str(nvar) + '.csv')
+    DF.to_csv(  scheduled_model + '999_NN_' + method + '_' + str(nvar) + '.csv' )
 
 
 
