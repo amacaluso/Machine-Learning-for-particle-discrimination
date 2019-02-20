61a6e7eb276543929fdf27417bb71edcabb7471c
amacaluso <antonio.macaluso2@unibo.it>
2019-02-20 15:40:25 +0100

    Descrizione variabili

--- 001_run_ML_missed.py
+++ 001_run_ML_missed.py
@@ -5,10 +5,9 @@ SEED = 741
 njob = 20
 
 
-
-
 methods = [ 'LASSO' ]
-nvars = [ 140, 160, 180, 200, 220, 240]
+nvars = [ 251 ]
+
 
 # predictors = extract_predictors( method, nvar, SEED)
 # eff_nvar = len(predictors)
@@ -27,83 +26,6 @@ for method in methods:
             exec(open("041_TREE_BASED_MODELS.py").read(), globals())
         except:
             DF.to_csv( scheduled_model + 'ERROR_TREE_BASED_' + method + '_' + str(nvar) + '.csv')
-
-        try:
-            exec(open("041_GBM.py").read(), globals())
-        except:
-            DF.to_csv( scheduled_model + 'ERROR_GBM_' + method + '_' + str(nvar) + '.csv')
-        try:
-            exec(open("041_TREE_BASED_MODELS.py").read(), globals())
-        except:
-            DF.to_csv( scheduled_model + 'ERROR_TREE_BASED_' + method + '_' + str(nvar) + '.csv')
-        try:
-            exec (open("051_NEURAL_NETWORK.py").read(), globals())
-        except:
-            DF.to_csv(scheduled_model + 'ERROR_NN_' + method + '_' + str(nvar) + '.csv')
-        try:
-            exec(open("045_BERNOULLI_NAIVE_BAYES.py").read(), globals())
-        except:
-            DF.to_csv( scheduled_model + 'ERROR_BERNOULLI_NAIVE_BAYES_' + method + '_' + str(nvar) + '.csv')
-
-
-
-methods = [ 'RANDOM_FOREST' ]
-nvars = [ 160, 180, 200, 220, 240]
-
-# predictors = extract_predictors( method, nvar, SEED)
-# eff_nvar = len(predictors)
-probs_to_check = np.arange(0.1, 0.91, 0.1)
-DF = pd.DataFrame()
-
-scheduled_model = 'running_model/'
-create_dir( scheduled_model)
-
-for method in methods:
-    for nvar in nvars:
-        predictors = extract_predictors(method, nvar, SEED)
-        eff_nvar = len(predictors)
-        print method, eff_nvar
-        # try:
-        #     exec(open("041_GBM.py").read(), globals())
-        # except:
-        #     DF.to_csv( scheduled_model + 'ERROR_GBM_' + method + '_' + str(nvar) + '.csv')
-        try:
-            exec(open("041_RANDOM_FOREST.py").read(), globals())
-        except:
-            DF.to_csv( scheduled_model + 'ERROR_RANDOM_FOREST_' + method + '_' + str(nvar) + '.csv')
-        try:
-            exec (open("051_NEURAL_NETWORK.py").read(), globals())
-        except:
-            DF.to_csv(scheduled_model + 'ERROR_NN_' + method + '_' + str(nvar) + '.csv')
-        try:
-            exec(open("041_TREE_BASED_MODELS.py").read(), globals())
-        except:
-            DF.to_csv( scheduled_model + 'ERROR_TREE_BASED_' + method + '_' + str(nvar) + '.csv')
-        try:
-            exec(open("045_BERNOULLI_NAIVE_BAYES.py").read(), globals())
-        except:
-            DF.to_csv( scheduled_model + 'ERROR_BERNOULLI_NAIVE_BAYES_' + method + '_' + str(nvar) + '.csv')
-
-
-
-
-
-methods = [ 'RIDGE' ]
-nvars = [ 120, 140, 160, 180, 200, 220, 240]
-
-# predictors = extract_predictors( method, nvar, SEED)
-# eff_nvar = len(predictors)
-probs_to_check = np.arange(0.1, 0.91, 0.1)
-DF = pd.DataFrame()
-
-scheduled_model = 'running_model/'
-create_dir( scheduled_model)
-
-for method in methods:
-    for nvar in nvars:
-        predictors = extract_predictors(method, nvar, SEED)
-        eff_nvar = len(predictors)
-        print method, eff_nvar
         try:
             exec(open("041_GBM.py").read(), globals())
         except:
@@ -112,36 +34,50 @@ for method in methods:
             exec(open("041_RANDOM_FOREST.py").read(), globals())
         except:
             DF.to_csv( scheduled_model + 'ERROR_RANDOM_FOREST_' + method + '_' + str(nvar) + '.csv')
-        try:
-            exec (open("051_NEURAL_NETWORK.py").read(), globals())
-        except:
-            DF.to_csv(scheduled_model + 'ERROR_NN_' + method + '_' + str(nvar) + '.csv')
-
-
-
-
-methods = [ 'RIDGE' ]
-nvars = [ 240 ]
-
-# predictors = extract_predictors( method, nvar, SEED)
-# eff_nvar = len(predictors)
-probs_to_check = np.arange(0.1, 0.91, 0.1)
-DF = pd.DataFrame()
-
-scheduled_model = 'running_model/'
-create_dir( scheduled_model)
-
-for method in methods:
-    for nvar in nvars:
-        predictors = extract_predictors(method, nvar, SEED)
-        eff_nvar = len(predictors)
-        print method, eff_nvar
         try:
             exec(open("043_REGULARIZED_METHODS.py").read(), globals())
-            DF.to_csv(scheduled_model + 'OK_REGULARIZED_METHODS' + method + '_' + str(nvar) + '.csv')
         except:
             DF.to_csv( scheduled_model + 'ERROR_LASSO_' + method + '_' + str(nvar) + '.csv')
         try:
             exec(open("045_BERNOULLI_NAIVE_BAYES.py").read(), globals())
         except:
             DF.to_csv( scheduled_model + 'ERROR_BERNOULLI_NAIVE_BAYES_' + method + '_' + str(nvar) + '.csv')
+        try:
+            exec(open("045_GAUSSIAN_NAIVE_BAYES.py").read(), globals())
+        except:
+            DF.to_csv( scheduled_model + 'ERROR_GAUSSIAN_NAIVE_BAYES_' + method + '_' + str(nvar) + '.csv')
+        try:
+            exec(open("046_KNN.py").read(), globals())
+        except:
+            DF.to_csv(  scheduled_model + 'ERROR_' + method + '_' + str(nvar) + '.csv' )
+        try:
+            exec (open("051_NEURAL_NETWORK.py").read(), globals())
+        except:
+            DF.to_csv(scheduled_model + 'ERROR_NN_' + method + '_' + str(nvar) + '.csv')
+
+
+
+
+
+
+methods = [ 'LASSO' ]
+nvars = [ 251 ]
+
+
+# predictors = extract_predictors( method, nvar, SEED)
+# eff_nvar = len(predictors)
+probs_to_check = np.arange(0.1, 0.91, 0.1)
+DF = pd.DataFrame()
+
+scheduled_model = 'running_model/'
+create_dir( scheduled_model)
+
+for method in methods:
+    for nvar in nvars:
+        predictors = extract_predictors(method, nvar, SEED)
+        eff_nvar = len(predictors)
+        print method, eff_nvar
+        try:
+            exec(open("041_GBM.py").read(), globals())
+        except:
+            DF.to_csv( scheduled_model + 'ERROR_GBM_' + method + '_' + str(nvar) + '.csv')
